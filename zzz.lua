6748e2007c23f421fb58375b1d94f13bb52670e3
AWDX-DYVB <150741018+AWDX-DYVB@users.noreply.github.com>
2024-11-21 03:15:21 +0800

    Update zzz.lua

--- zzz.lua
+++ zzz.lua
@@ -1,1 +1,1 @@
-@print("测试")@
+@adapter.add {漫画名字 = "vvvNo resources were found. Please be patient while the resources are updated",漫画封面 = "",动漫评分 = "评价分数 [未评价]",漫画类型 = "Empty Shell",漫画语言页数 = "CN 0P",漫画发布时间 = "9999-99-99 99:99"动漫作者名字 = "bgyLenfkrb",}@
